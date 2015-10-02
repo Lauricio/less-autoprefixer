ab8c998878ca2b2b39aecb49fe846a47f55f9ced
Lauricio Su <me@lauricio.com>
2015-10-03 00:16:50 +0700

    added more reliable test for generated stylesheet

--- autoprefixer_tests.js
+++ autoprefixer_tests.js
@@ -1,4 +1,4 @@
-Tinytest.add("autoprefixer - prefixing", function (test) {
+Tinytest.add("autoprefixer - document.styleSheets", function (test) {
 
   var stylesheets = document.styleSheets;
   var stylesheetsList = Object.keys(stylesheets);
@@ -32,3 +32,30 @@ Tinytest.add("autoprefixer - prefixing", function (test) {
 
   }
 });
+
+var styleSheets = document.querySelectorAll('[rel="stylesheet"]');
+var autoprefixerUrl = '';
+
+// Get autoprefixer stylesheet url
+for (var i = styleSheets.length - 1; i >= 0; i--) {
+  var url = styleSheets[i].href;
+  if (new RegExp('autoprefixer.less.css').test(url)) {
+    autoprefixerUrl = url;
+  }
+}
+
+testAsyncMulti("autoprefixer - stylesheet", [
+
+  function (test, expect) {
+    $.get(autoprefixerUrl, expect(function (res) {
+      test.equal(/-webkit-user-select|-moz-user-select|-ms-user-select/.test(res), true);
+    }))
+  },
+  function (test, expect) {
+    $.get(autoprefixerUrl, expect(function (res) {
+      test.equal(/-webkit-box-pack|-webkit-justify-content|-ms-flex-pack/.test(res), true);
+    }))
+  }
+
+]);
+
--- package.js
+++ package.js
@@ -24,7 +24,7 @@ Package.onUse(function (api) {
 
 Package.onTest(function(api) {
   api.use('lauricio:less-autoprefixer');
-  api.use(['tinytest', 'test-helpers']);
+  api.use(['tinytest', 'test-helpers','jquery']);
   api.addFiles(['tests/top.import.less',
                 'tests/top3.import.less',
                 'tests/imports/not-included.less',
