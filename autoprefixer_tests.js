bcf41717a4c47290193b9cdbb5304926b23c4a1a
Lauricio Su <me@lauricio.com>
2015-10-03 14:06:31 +0700

    cleaned up Autoprefixer test

--- autoprefixer_tests.js
+++ autoprefixer_tests.js
@@ -4,7 +4,7 @@ var autoprefixerUrl = '';
 // Get autoprefixer stylesheet url
 for (var i = styleSheets.length - 1; i >= 0; i--) {
   var url = styleSheets[i].href;
-  if (new RegExp('autoprefixer.less.css').test(url)) {
+  if (url.match(/autoprefixer.less.css/)) {
     autoprefixerUrl = url;
   }
 }
@@ -12,13 +12,13 @@ for (var i = styleSheets.length - 1; i >= 0; i--) {
 testAsyncMulti("autoprefixer - stylesheet", [
 
   function (test, expect) {
-    $.get(autoprefixerUrl, expect(function (res) {
-      test.equal(/-webkit-user-select|-moz-user-select|-ms-user-select/.test(res), true);
+    $.get(autoprefixerUrl, expect(function (stylesheet) {
+      test.equal(/-webkit-user-select|-moz-user-select|-ms-user-select/.test(stylesheet), true);
     }))
   },
   function (test, expect) {
-    $.get(autoprefixerUrl, expect(function (res) {
-      test.equal(/-webkit-box-pack|-webkit-justify-content|-ms-flex-pack/.test(res), true);
+    $.get(autoprefixerUrl, expect(function (stylesheet) {
+      test.equal(/-webkit-box-pack|-webkit-justify-content|-ms-flex-pack/.test(stylesheet), true);
     }))
   }
 
